415f274a47aa22192ac719ef61a0bae0a3cc88ee
Mauricio <mauriciosolorio@nevada.unr.edu>
2020-09-11 12:02:13 -0700

    Last modification

--- HW1.py
+++ HW1.py
@@ -22,22 +22,22 @@ df2 = df.loc[(df.empstat == 'employed') & (df.age >= 30) & (df.age <= 39)]
 print(df2.shape)  # '(14517, 5) Number of rows and columns'
 
 # (a) Find the avergae income of these population set
-muinc = df2['incwage'].mean()  # '$42,112.44 average income'
+muinc = df2['incwage'].mean()  # '$48,348.34 average income'
 print(muinc.round(2))
 
 # (b) Take a random sample of 1000 women, random state is used to ensure the
 # code chooses the same random samples everytime it runs.
 df3 = df2.sample(n=1000, random_state=23)
 # (i) Sample mean
-smu = df3.incwage.mean()  # '$43,402.09 average income sample'
+smu = df3.incwage.mean()  # '$47,355.63 average income sample'
 print(smu.round(2))
 
 # (ii) Sample Standard Deviation
-sigma = df3.incwage.std()  # '$39,111.74'
+sigma = df3.incwage.std()  # '$39,169.62'
 print(sigma.round(2))
 
 # (iii) Standard Error
-SE = sigma/np.sqrt(1000)  # '1236.82'
+SE = sigma/np.sqrt(1000)  # '1238.65'
 print(SE.round(2))
 
 # (iv) Give the 95% confidence interval on your estimate of μ.
@@ -49,17 +49,17 @@ t = stats.t.ppf(1-0.025, 999)  # 'From stats package which is same as table'
 
 CI95 = SE*t  # '+/- 2427.06661315927'
 # Lower end of the range
-print((smu-CI95).round(2))  # '40975.02'
+print((smu-CI95).round(2))  # '44924.97'
 
 # Upper end of the range
-print((smu+CI95).round(2))  # '45829.16'
+print((smu+CI95).round(2))  # '49786.29'
 
 # (v) Ho m0 = 50,000 ; H1 m1 =! 50,000
-ttest = (smu - 50000)/SE  # '5.33 which is greater than t, thus reject null'
-# alpha is ifinitibly small as well
+ttest = (smu - 50000)/SE  # '2.13 which is greater than t, thus reject null'
+# alpha between 0.025 & 0.01
 print(ttest.round(2))
 # (vi) Ho m = 55,000 ; H1 m =! 50000
-ttest = (smu - 55000)/SE  # '9.38 thus reject Ho and alpha is very small'
+ttest = (smu - 55000)/SE  # '6.17 thus reject Ho and alpha is very small'
 print(ttest.round(2))
 
 
@@ -81,8 +81,8 @@ plt.xlabel('Number of samples')
 plt.show()
 
 # (ii) Report the average of these 10,000 sample means.
-ave = arr.mean()  # '$42,110.21'
+ave = arr.mean()  # '$48329.9'
 print(ave.round(2))
 # (iii) Report the standard deviation of these 10,000 sample means.
-sig = arr.std()  # '1090.13'
+sig = arr.std()  # '1470.4'
 print(sig.round(2))
