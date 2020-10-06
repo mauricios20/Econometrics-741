5b2459a0f3e47219549b66555aa5903ab23ec961
Mauricio <mauriciosolorio@nevada.unr.edu>
2020-10-06 16:11:04 -0700

    10/06/2020

--- HW2.py
+++ HW2.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import statsmodels.api as sm
+import matplotlib.pyplot as plt
 # Start by importing the data into python to create a data frame (df) #
 df = pd.read_stata('C:/Users/mauri/github/Econometrics-741/HW2.dta')
 
@@ -80,7 +81,7 @@ df3 = df.loc[(df.empstat == 'employed') & (df.age >= 35)]
 df3['lnwage'] = np.log(df3.incwage)
 df3['age2'] = np.power((df3.age), 2)
 df3['age3'] = np.power((df3.age), 3)
-print(df3.head(3))
+df3['MFX'] = np.array(df3['age']+2*df3['age2']*df3['age']+3*df3['age3']*df3['age2'])
 
 # a) Run a linear regression of earnings on age.
 x = df3[['age']]
@@ -91,12 +92,13 @@ N = len(x)
 p = len(x.columns) + 1  # plus one because LinearRegression adds an intercept term
 
 
-X_with_intercept = np.empty(shape=(N, p), dtype=np.float)
-X_with_intercept[:, 0] = 1
-X_with_intercept[:, 1:p] = x.values
+X_with_intercept1 = np.empty(shape=(N, p), dtype=np.float)
+X_with_intercept1[:, 0] = 1
+X_with_intercept1[:, 1:p] = x.values
 
-ols = sm.OLS(y.values, X_with_intercept)
+ols = sm.OLS(y.values, X_with_intercept1)
 ols_result_ln = ols.fit()
+y_hat1 = ols_result_ln.predict(X_with_intercept1)
 print(ols_result_ln.summary())
 
 #  (b) Run a quadratic regression of earnings on age. (4 points)
@@ -108,12 +110,13 @@ N = len(x)
 p = len(x.columns) + 1  # plus one because LinearRegression adds an intercept term
 
 
-X_with_intercept = np.empty(shape=(N, p), dtype=np.float)
-X_with_intercept[:, 0] = 1
-X_with_intercept[:, 1:p] = x.values
+X_with_intercept2 = np.empty(shape=(N, p), dtype=np.float)
+X_with_intercept2[:, 0] = 1
+X_with_intercept2[:, 1:p] = x.values
 
-ols = sm.OLS(y.values, X_with_intercept)
-ols_result_ln = ols.fit()
+ols = sm.OLS(y.values, X_with_intercept2)
+ols_result_ln2 = ols.fit()
+y_hat2 = ols_result_ln2.predict(X_with_intercept2)
 print(ols_result_ln.summary())
 
 #  (c) Run a cubic regression of earnings on age.
@@ -125,10 +128,40 @@ N = len(x)
 p = len(x.columns) + 1  # plus one because LinearRegression adds an intercept term
 
 
-X_with_intercept = np.empty(shape=(N, p), dtype=np.float)
-X_with_intercept[:, 0] = 1
-X_with_intercept[:, 1:p] = x.values
+X_with_intercept3 = np.empty(shape=(N, p), dtype=np.float)
+X_with_intercept3[:, 0] = 1
+X_with_intercept3[:, 1:p] = x.values
 
-ols = sm.OLS(y.values, X_with_intercept)
-ols_result_ln = ols.fit()
-print(ols_result_ln.summary())
+ols = sm.OLS(y.values, X_with_intercept3)
+ols_result_ln3 = ols.fit()
+y_hat3 = ols_result_ln3.predict(X_with_intercept3)
+print(ols_result_ln3.summary())
+# (d) Present the results from your three regressions in one graph. (4
+# points)
+df4 = pd.DataFrame(list(zip(df3['age'], y_hat1, y_hat2, y_hat3, df3['lnwage'])),
+                   columns=['age', 'yhat1', 'yhat2', 'yhat3', 'lnwage'])
+
+df5 = df4.groupby(['age'], as_index=False).mean()
+
+plt.scatter(df5['age'], df5['lnwage'],  color='black')
+plt.plot(df5['age'], df5['yhat1'], color='blue', label='Linear')
+plt.plot(df5['age'], df5['yhat2'], color='green', label='Quadratic')
+plt.plot(df5['age'], df5['yhat3'], color='red', label='Cubic')
+plt.legend()
+plt.ylabel('Mean Log Wage')
+plt.xlabel('Age')
+plt.show()
+
+
+# (e) Make an argument about which specification is best. (6 points)
+# In this case just by looking at the graph, the cubic regression seems
+# to fit the data better than the others. After analyzing the $r^2$
+# coefficients of each regression, it can be noticed that the cubic regression
+# does have the greatest value, which indicates a better positive fit.
+# (f) Using the cubic, let’s explore marginal effects.
+agebar = np.mean(df3['age'])
+age2bar = np.mean(df3['age2'])
+
+df3['MFX_bar'] = np.array(df3['age']+2*df3['age2']*agebar+3*df3['age3']*age2bar)
+print(df3['MFX'].describe())
+print(df3['MFX_bar'].describe())
